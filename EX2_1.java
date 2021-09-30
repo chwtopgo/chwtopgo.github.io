9f38de56a372ecbd0de3f74812f50caf6cd0a257
chwtopgo <80186649+chwtopgo@users.noreply.github.com>
2021-09-30 16:27:15 +0900

    Create EX2_1.java

---
+++ EX2_1.java
@@ -0,0 +1,21 @@
+package org.javaro.lecture;
+public class EX2_1{
+public static void main(String[]args){
+  int secondVar=1000;
+  int minOut=secondVar/60;
+  int secondOut=secondVar%60
+float a=3.5f;
+  a=a+2.1f;
+  System.out.println("a=3.5;a=a+2.1;a="+a);
+  a+=2.1;
+    System.out.println("a=5.6;a=a+2.1;a="+a);
+  a-=2.1;
+  System.out.println("a=7.7;a=a-2.1;a="+a);
+  a*=2;
+    System.out.println("a=5.6;a=a*2;a="+a);
+  a/=2;
+    System.out.println("a=11.2;a=a/2;a="+a);
+  a%=2;
+    System.out.println("a=5.6;a=a%2;나머지="+a);
+}
+}
